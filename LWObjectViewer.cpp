697e48ea2dac47052a1cb2fc8cb2bc366a653ad5
Bryan <31681524+laubryan@users.noreply.github.com>
2023-01-20 11:19:00 -0500

    Move prototype declarations to header file

--- LWObjectViewer.cpp
+++ LWObjectViewer.cpp
@@ -25,19 +25,6 @@ UINT RENDER_WINDOW_HEIGHT = 768;
 bool objectLoaded = false;
 bool tumbling = true;
 
-// Forward declarations of functions included in this code module:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-
-// Initialization functions
-void	CreateMainWindowControls();
-
-// Event handlers
-void	HandleDroppedFile(HDROP dropInfo);
-void	HandleMouseWheel(short wheelDelta);
-
 // Renderer class
 Renderer renderer;
 
--- LWObjectViewer.h
+++ LWObjectViewer.h
@@ -3,5 +3,19 @@
 #include "resource.h"
 #include <shellapi.h>
 
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+// Initialization functions
+void	CreateMainWindowControls();
+
+// Event handlers
+void	HandleDroppedFile(HDROP dropInfo);
+void	HandleMouseWheel(short wheelDelta);
+
+
 // Control IDs
 #define IDC_ROTATE WM_USER + 1
